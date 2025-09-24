d4ed5fc6d405b3260b0343ef76dd3bb97b89ebf0
Erik Olsson <olsson.erik1993@gmail.com>
2025-09-24 09:31:08 +0200

    problem json

--- order-service/src/main/java/com/github/mangila/resilience/orderservice/RestErrorHandler.java
+++ order-service/src/main/java/com/github/mangila/resilience/orderservice/RestErrorHandler.java
@@ -1,20 +1,29 @@
 package com.github.mangila.resilience.orderservice;
 
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Map;
 
 @RestControllerAdvice
 public class RestErrorHandler {
 
+    /**
+     * Handle the exception thrown when the circuit breaker is open.
+     */
     @ExceptionHandler(CallNotPermittedException.class)
-    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    public Map<String, String> handleNotFound(CallNotPermittedException ex) {
-        return Map.of("message", ex.getMessage());
+    public ProblemDetail handleCallNotPermittedException(CallNotPermittedException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage());
     }
 
+
+    /**
+     * Handle the exception thrown when the rate limiter is overloaded.
+     */
+    @ExceptionHandler(RequestNotPermitted.class)
+    public ProblemDetail handleRequestNotPermittedException(RequestNotPermitted ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage());
+    }
 }
